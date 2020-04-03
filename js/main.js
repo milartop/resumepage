1b6d70b620886ada5de6d27923cb248b3fdb0c48
milart61 <48012045+milart61@users.noreply.github.com>
2020-04-03 21:25:57 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,9 +1,14 @@
 $(document).ready(function () {
+  var navLink = $('li a');
+
   $('.menu-burger').click(function (event) {
+
     $('.menu-wrap, .menu-burger').toggleClass('active');
 
   });
   $("li").children().click(function (event) {
     $('.menu-wrap, .menu-burger').removeClass('active');
   });
+
+
 });
